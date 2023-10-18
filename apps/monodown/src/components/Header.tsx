d83386735cc8cd8ef61f43610bb79cda324d9b32
Kyle Zheng <kylezheng73@gmail.com>
2023-10-18 03:49:39 -0400

    dark mode toggle

--- apps/monodown/src/components/Header.tsx
+++ apps/monodown/src/components/Header.tsx
@@ -1,5 +1,8 @@
 import { A } from "solid-start";
 import Icon from "../assets/svg/icon.svg";
+import { Switch } from "@kobalte/core";
+import { createEffect, createSignal } from "solid-js";
+
 export function Header() {
   return (
     <header class="px-8 py-2 flex justify-between">
@@ -12,7 +15,58 @@ export function Header() {
         <A href="./test">Test</A>
         <A href="./components">Components</A>
         <A href="./dungeon">Dungeon</A>
+        <DarkModeToggle />
       </div>
     </header>
+  );
+}
+
+const getTheme = () => {
+  // We guess on the server, but if it differs, there will be a flicker between render and hydration
+  if (typeof localStorage === "undefined") return "light";
+
+  const saved = localStorage.getItem("theme");
+  if (saved) return saved;
+
+  if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
+    return "dark";
+  } else {
+    return "light";
+  }
+};
+
+function DarkModeToggle() {
+  const [darkMode, setDarkMode] = createSignal(getTheme() === "dark");
+
+  createEffect(() => {
+    const body = document.body;
+    if (darkMode()) {
+      body.classList.add("dark");
+      localStorage.setItem("theme", "dark");
+    } else {
+      body.classList.remove("dark");
+      localStorage.setItem("theme", "light");
+    }
+  });
+
+  return (
+    <Switch.Root
+      class="inline-flex items-center"
+      checked={darkMode()}
+      onChange={setDarkMode}
+    >
+      {/* <Switch.Label class="mr-[6px] select-none text-sm">
+        Dark Mode
+      </Switch.Label> */}
+      <Switch.Input class="peer" />
+      <Switch.Control class="inline-flex items-center bg-stone-200 border rounded-[12px] h-[24px] w-[46px] px-[2px] peer-focus:(ring-2 ring-ring ring-offset-2 ring-offset-background) data-[checked]:(border bg-stone-800)">
+        <Switch.Thumb class="inline-flex items-center h-[20px] w-[20px] rounded-[10px] transition-transform bg-background data-[checked]:translate-x-full">
+          <div
+            class="m-auto p-[2px]"
+            classList={{ "i-uil:moon": darkMode(), "i-uil:sun": !darkMode() }}
+          ></div>
+        </Switch.Thumb>
+      </Switch.Control>
+    </Switch.Root>
   );
 }
