461163d5461df1a3c8564357231218683ba818da
Kyle Zheng <kylezheng73@gmail.com>
2023-11-01 21:43:25 -0400

    solid-marked working w/ custom components + toc + nav

--- apps/monodown/src/routes/(site).tsx
+++ apps/monodown/src/routes/(site).tsx
@@ -1,7 +1,8 @@
 import { Outlet } from "solid-start";
 import { Header } from "~/components/Header";
 import { MDXProvider } from "solid-marked";
-import { Dynamic } from "solid-js/web";
+import { Dynamic, Show } from "solid-js/web";
+import { JSX } from "solid-js";
 
 export default function SiteLayout() {
   return (
@@ -15,25 +16,70 @@ export default function SiteLayout() {
           },
           Heading(props) {
             return (
-              <Dynamic component={`h${props.depth}`}>{props.children}</Dynamic>
+              <Dynamic
+                component={`h${props.depth}`}
+                class="font-bold"
+                classList={{
+                  "text-5xl": props.depth === 1,
+                  "text-4xl": props.depth === 2,
+                  "text-3xl": props.depth === 3,
+                  "text-2xl": props.depth === 4,
+                  "text-xl": props.depth === 5,
+                  "text-lg": props.depth === 6,
+                }}
+              >
+                {props.children}
+              </Dynamic>
             );
           },
           Paragraph(props) {
             return <p>{props.children}</p>;
           },
+          Image(props) {
+            return <img src={props.url} alt={props.alt ?? props.title} />;
+          },
+          Strong(props) {
+            return <span class="font-bold">{props.children}</span>;
+          },
+          Emphasis(props) {
+            return <span class="font-italic">{props.children}</span>;
+          },
+          List(props) {
+            return (
+              <Dynamic component={props.ordered ? 'ol' : 'ul'} start={props.start ?? undefined}>
+                {props.children}
+              </Dynamic>
+            );
+          },
+          ListItem(props) {
+            return (
+              <li>
+                <Show when={'checked' in props} fallback={props.children}>
+                  <input type="checkbox" checked={props.checked ?? undefined} />
+                  {props.children}
+                </Show>
+              </li>
+            );
+          },
+          Link(props) {
+            return (
+              <a href={props.url} title={props.title ?? undefined}>{props.children}</a>
+            );
+          },
+        }}
+        components={{
+          Example() {
+            return <div>example worked</div>;
+          },
+          Content(props: {children: JSX.Element}) {
+            return <div>{props.children}</div>
+          }
         }}
       >
-        {/* <MDXProvider
-        components={{
-          h1: (props: HProps) => <h1 class="text-5xl font-bold" {...props} />,
-          h2: (props: HProps) => <h2 class="text-4xl font-bold" {...props} />,
-          h3: (props: HProps) => <h3 class="text-3xl font-bold" {...props} />,
-          h4: (props: HProps) => <h4 class="text-2xl font-bold" {...props} />,
-          h5: (props: HProps) => <h5 class="text-xl font-bold" {...props} />,
-          h6: (props: HProps) => <h6 class="text-lg font-bold" {...props} />,
-        }}
-      > */}
-        <Outlet />
+        <div class="flex">
+        <div>navigation</div>
+          <Outlet />
+        </div>
       </MDXProvider>
     </>
   );
