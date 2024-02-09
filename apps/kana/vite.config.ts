f4ebd0babd7cb9fe4dbc5bc9f515059a9dc8d73e
Kyle Zheng <kylezheng73@gmail.com>
2024-02-08 22:49:43 -0500

    fix vercel build

--- apps/kana/vite.config.ts
+++ apps/kana/vite.config.ts
@@ -2,6 +2,10 @@ import { defineConfig } from "@solidjs/start/config";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  start: { ssr: false },
+  start: {
+    server: {
+      preset: "vercel",
+    },
+  },
   plugins: [tsconfigPaths({ root: "./" })],
 });
